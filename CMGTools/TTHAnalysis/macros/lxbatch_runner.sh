52d21fefe9ecb6b04823fe002c3b9077e13de204
Henning Kirschenmann <henning.kirschenmann@cern.ch>
2015-02-18 16:50:10 +0100

    set environment variable such that forked eos-read processes don't take forever/are impossible

--- CMGTools/TTHAnalysis/macros/lxbatch_runner.sh
+++ CMGTools/TTHAnalysis/macros/lxbatch_runner.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 export SCRAM_ARCH=slc6_amd64_gcc481
+#following suggestion on https://savannah.cern.ch/bugs/?100688 to access eos in multiple instances
+export XRD_ENABLEFORKHANDLERS=1
 WORK=$1; shift
 SRC=$1; shift
 cd $SRC; 
