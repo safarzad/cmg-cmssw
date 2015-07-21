115035f48796f5a96925d67bfd1e96255650067d
Artur Lobanov <artur.lobanov@desy.de>
2015-07-21 19:09:41 +0200

    Update trigger vars

--- CMGTools/SUSYAnalysis/python/tools/eventVars_1l_triggers.py
+++ CMGTools/SUSYAnalysis/python/tools/eventVars_1l_triggers.py
@@ -14,7 +14,9 @@ class EventVars1L_triggers:
             'HLT_SingleMu', 'HLT_Mu45NoIso', 'HLT_Mu50NoIso', 'HLT_MuHad',
             'HLT_MuHT600', 'HLT_MuHT400MET70','HLT_MuHT350MET70','HLT_MuMET120', 'HLT_MuHT400B',
             'HLT_SingleEl', 'HLT_ElNoIso', 'HLT_ElHad',
-            'HLT_EleHT600','HLT_EleHT400MET70','HLT_EleHT350MET70','HLT_EleHT200', 'HLT_ElHT400B'
+            'HLT_EleHT600','HLT_EleHT400MET70','HLT_EleHT350MET70','HLT_EleHT200', 'HLT_ElHT400B',
+            ## custom names
+            'HLT_IsoMu27','HLT_IsoEle32'
             ]
 
     def listBranches(self):
@@ -26,44 +28,21 @@ class EventVars1L_triggers:
         ret = {}
 
         '''
-        ret['HLT_HT350'] = event.HLT_HT350
-        ret['HLT_HT600'] = event.HLT_HT600
-        ret['HLT_HT900'] = event.HLT_HT900
-        ret['HLT_MET170'] = event.HLT_MET170
-        ret['HLT_HTMET'] = event.HLT_HTMET
-        ret['HLT_Had'] = event.HLT_Had
-
-        ret['HLT_SingleMu'] = event.HLT_SingleMu
-        ret['HLT_MuNoIso'] = event.HLT_MuNoIso
-        ret['HLT_MuHad'] = event.HLT_MuHad
-
-        ret['HLT_MuHT600'] = event.HLT_MuHT600
-        ret['HLT_MuMET120'] = event.HLT_MuMET120
-        ret['HLT_MuHT400MET70'] = event.HLT_MuHT400MET70
-        ret['HLT_MuHT400B'] = event.HLT_MuHT400B
-
-        ret['HLT_SingleEl'] = event.HLT_SingleEl
-        ret['HLT_ElNoIso'] = event.HLT_ElNoIso
-        ret['HLT_ElHad'] = event.HLT_ElHad
-
-        ret['HLT_EleHT200'] = event.HLT_EleHT200
-        ret['HLT_EleHT400MET70'] = event.HLT_EleHT400MET70
-        ret['HLT_EleHT600'] = event.HLT_EleHT600
-        ret['HLT_ElHT400B'] = event.HLT_ElHT400B
-        '''
-
-        ## replaces ^^^ list above (and saver as well)
         ## check that any HLT branch exists in tree
-        if not (hasattr(event,'HLT_SingleMu') or  hasattr(event,'HLT_SingleEl')):
-            #print 'Has no trigger info!'
-            return ret
+        if not (hasattr(event,'HLT_SingleMu') or hasattr(event,'HLT_SingleEl')):
+        #print 'Has no trigger info!'
+        return ret
+        '''
 
         ## loop over all HLT names and set them in tree
         for var in self.branches:
             #print var, getattr(event,var)
-
-            if 'HLT_' in var:
+            if 'HLT_' in var and hasattr(event,var):
                 ret[var] = getattr(event,var)
+
+        # custom names fro triggers
+        ret['IsoMu27'] = event.HLT_SingleMu
+        ret['IsoEle32'] = event.HLT_SingleEl
 
         # return branches
         return ret
