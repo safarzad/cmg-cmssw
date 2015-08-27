34bb9b996aa29ae22d85510efaf5c82cb569a920
Artur Lobanov <artur.lobanov@desy.de>
2015-08-27 19:14:44 +0200

    Updates for Rcs macro

--- CMGTools/TTHAnalysis/python/plotter/susy-1lep/RcsDevel/makeRcsCardsSusy.py
+++ CMGTools/TTHAnalysis/python/plotter/susy-1lep/RcsDevel/makeRcsCardsSusy.py
@@ -3,9 +3,13 @@ from CMGTools.TTHAnalysis.plotter.mcAnalysis import *
 import re, sys, os, os.path
 systs = {}
 
+from searchBins import *
+
 # trees
-Tdir = "/nfs/dust/cms/group/susy-desy/Run2/ACDV/CMGtuples/MC/SPRING15/Spring15/Links/"
-FTdir = "/nfs/dust/cms/group/susy-desy/Run2/ACDV/CMGtuples/MC/SPRING15/Spring15/Links/Friends/"
+#Tdir = "/nfs/dust/cms/group/susy-desy/Run2/ACDV/CMGtuples/MC/SPRING15/Spring15/Links/"
+#FTdir = "/nfs/dust/cms/group/susy-desy/Run2/ACDV/CMGtuples/MC/SPRING15/Spring15/Links/Friends/"
+Tdir = "/nfs/dust/cms/group/susy-desy/Run2/MC/CMGtuples/Spring15/"
+FTdir = "/nfs/dust/cms/group/susy-desy/Run2/MC/CMGtuples/Spring15/Friends/"
 
 def addOptions(options):
 
@@ -19,7 +23,7 @@ def addOptions(options):
     options.weight = True
     options.final  = True
     options.allProcesses  = True
-    #options.maxEntries = 100
+    options.maxEntries = 10
 
 
 def analyzeReport(options):
@@ -29,30 +33,53 @@ def analyzeReport(options):
     cuts = CutsFile(options.cutFile,options)
 
     # make bin name and outdir names
+
     #binname = os.path.basename(options.cutFile).replace(".txt","") if options.outname!=None else options.outname
-    binname = "test"
+    binname = options.bin
+    if options.outdir == None:
+        outdir = "test/"
+    else:
+        outdir = options.outdir
     #outdir  = options.outdir+"/" if options.outdir!=None else ""
-    outdir = "test/"
+    #outdir = "test/"
 
     # get report
-    report = mca.getPlotsRaw(binname, options.var, options.bins, cuts.allCuts(), nodata=options.asimov)
+    if options.pretend:
+        report = []
+    else:
+        report = mca.getPlotsRaw("x", options.var, options.bins, cuts.allCuts(), nodata=options.asimov)
 
+    # add sum MC entry
+    if not options.pretend:
+        totalMC = []
+        for p in mca.listBackgrounds():
+            if p in report: totalMC.append(report[p])
+            report['x_background'] = mergePlots("x_background", totalMC)
+
+    '''
     if options.asimov:
         tomerge = []
         for p in mca.listBackgrounds():
             if p in report: tomerge.append(report[p])
         report['data_obs'] = mergePlots(binname+"_data_obs", tomerge)
-    else:
-        report['data_obs'] = report['data'].Clone(binname+"_data_obs")
+    #else:
+    #    report['data_obs'] = report['data'].Clone(binname+"_data_obs")
+    '''
 
     myout = outdir+"/common/"
     if not os.path.exists(myout): os.system("mkdir -p "+myout)
 
-    workspace = ROOT.TFile.Open(myout+binname+".input.root", "RECREATE")
-    #workspace = ROOT.TFile.Open("input.root", "RECREATE")
-    for n,h in report.iteritems():
-        if options.verbose > 0: print "\t%s (%8.3f events)" % (h.GetName(),h.Integral())
-        workspace.WriteTObject(h,h.GetName())
+    foutname = myout+binname+".yields.root"
+    workspace = ROOT.TFile.Open(foutname, "RECREATE")
+    if options.verbose > 0:
+        print 'Writing', foutname
+        print 'Yields:'
+
+    if not options.pretend:
+        for n,h in report.iteritems():
+            if options.verbose > 0:
+                print "\t%s (%8.3f events)" % (h.GetName(),h.Integral())
+            workspace.WriteTObject(h,h.GetName())
     workspace.Close()
 
 
@@ -64,7 +91,8 @@ def makeCards(options, args):
         print options
 
     if options.pretend:
-        exit(0)
+        #return 1
+        pass
 
     analyzeReport(options)
 
@@ -85,10 +113,11 @@ if __name__ == "__main__":
 
     # extra options for tty
     parser.add_option("-m","--mca", dest="mcaFile",default="mca-Cards_test.txt",help="MCA sample list")
-    parser.add_option("-c","--cuts", dest="cutFile",default="cards_cf.txt",help="Baseline cuts file")
+    parser.add_option("-c","--cuts", dest="cutFile",default="trig_base.txt",help="Baseline cuts file")
+    parser.add_option("--binname", dest="binname",default="test",help="Binname")
 
     # options for cards
-    parser.add_option("--var", dest="var",default="1",help="Var (Expr)")
+    parser.add_option("--var", dest="var",default="1",help="Variable")
     parser.add_option("--bins", dest="bins",default="[0.5,1.5]",help="Histo Bins")
 
     # I/O options
@@ -107,7 +136,7 @@ if __name__ == "__main__":
     #parser.add_option("-f","--force", dest="force",default=False, action="store_true",help="force mode")
 
     #makeShapeCards options
-    parser.add_option("--asimov", dest="asimov", action="store_true", default=True, help="Make Asimov pseudo-data")
+    parser.add_option("--asimov", dest="asimov", action="store_true", default=False, help="Make Asimov pseudo-data")
     #parser.add_option("--dummy",  dest="dummyYieldsForZeroBkg", action="store_true", default=False, help="Set dummy yields such it corresponds to 0.01 for 4/fb");
     #parser.add_option("--ignoreEmptySignal",  dest="ignoreEmptySignal", action="store_true", default=False, help="Do not write out a datacard if the expected signal is less than 0.01");
 
@@ -117,4 +146,46 @@ if __name__ == "__main__":
     if options.verbose > 0:
         print 'Arguments', args
 
-    makeCards(options, args)
+    for lt_bin,lt_cut in binsLT.iteritems():
+
+        htbins = []
+
+        if lt_bin in ['LT1']:
+            htbins += ['HT0','HT1p2']
+        if lt_bin in ['LT1','LT2']:
+            htbins += ['HT0']
+        if lt_bin in ['LT2']:
+            htbins += ['HT1']
+        if lt_bin in ['LT2','LT3','LT4']:
+            htbins += ['HT2']
+        if lt_bin in ['LT3','LT4']:
+            htbins += ['HT0p1']
+
+        #for ht_bin,ht_cut in binsHT.iteritems():
+        for ht_bin in htbins:
+            ht_cut = binsHT[ht_bin]
+
+            nbbins = []
+
+            # Match NB bins
+            if lt_bin in ['LT1','LT2','LT3','LT4']:
+                nbbins += ['NB1'] # NB1 present in all LT bins
+            if lt_bin in ['LT1','LT2','LT3']:
+                nbbins += ['NB2','NB3'] # NB1 present in all LT bins
+            if lt_bin in ['LT4']:
+                nbbins += ['NB2p3'] # NB1 present in all LT bins
+
+            for nb_bin in nbbins:
+                nb_cut = binsNB[nb_bin]
+            #for nb_bin,nb_cut in binsNB.iteritems():
+
+                binname = "%s_%s_%s" %(lt_bin,ht_bin,nb_bin)
+                options.cutsToAdd = [("triggLT",lt_bin,lt_cut),("triggLT",ht_bin,ht_cut),("triggLT",nb_bin,nb_cut)]
+
+                if options.verbose > 0:
+                    print 80*'#'
+                    print 'Processing bin %s with cuts %s' %(binname,options.cutsToAdd)
+
+                options.bin = binname
+                #print options
+                makeCards(options, args)
--- CMGTools/TTHAnalysis/python/plotter/susy-1lep/RcsDevel/searchBins.py
+++ CMGTools/TTHAnalysis/python/plotter/susy-1lep/RcsDevel/searchBins.py
@@ -1,8 +1,28 @@
 # LT bins
-dictLT = {}
+binsLT = {}
+binsLT['LT1'] = '250 < LT && LT < 350'
+binsLT['LT2'] = '350 < LT && LT < 450'
+binsLT['LT3'] = '450 < LT && LT < 600'
+binsLT['LT4'] = '600 < LT'
 
-dictLT['LT1'] = '250 < LT && LT < 350'
-dictLT['LT2'] = '350 < LT && LT < 450'
-dictLT['LT3'] = '450 < LT && LT < 600'
+# HT bins
+binsHT = {}
+binsHT['HT0'] = '500 < HT && HT < 750'
+binsHT['HT1'] = '750 < HT && HT < 1250'
+binsHT['HT2'] = '1250 < HT'
+binsHT['HT1p2'] = '750 < HT'
+binsHT['HT0p1'] = '500 < HT && HT < 1250'
 
-binsLT = ['LT1','LT2','LT3']
+#NB bins
+binsNB = {}
+#binsNB['NB0'] = 'nBJet == 0'
+binsNB['NB1'] = 'nBJet == 1'
+binsNB['NB2'] = 'nBJet == 2'
+binsNB['NB3'] = 'nBJet >= 3'
+binsNB['NB2p3'] = 'nBJet >= 2'
+
+
+# old
+cutDict = {}
+cutDict['HT1'] = ' -R "HT HT500 HT>500"'
+cutDict['HT2'] = ' -R "HT HT1000 HT>1000"'
