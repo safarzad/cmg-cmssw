9527df3507e491ccc4d35d3eb6ba5b5ffccb3ce0
Artur Lobanov <artur.lobanov@desy.de>
2015-08-28 13:29:47 +0200

    Update bins and yield macro

--- CMGTools/TTHAnalysis/python/plotter/susy-1lep/RcsDevel/makeBinYields.py
+++ CMGTools/TTHAnalysis/python/plotter/susy-1lep/RcsDevel/makeBinYields.py
@@ -10,6 +10,7 @@ from searchBins import *
 #FTdir = "/nfs/dust/cms/group/susy-desy/Run2/ACDV/CMGtuples/MC/SPRING15/Spring15/Links/Friends/"
 Tdir = "/nfs/dust/cms/group/susy-desy/Run2/MC/CMGtuples/Spring15/"
 FTdir = "/nfs/dust/cms/group/susy-desy/Run2/MC/CMGtuples/Spring15/Friends/"
+#FTdir = "FriendTrees_MC/"
 
 def addOptions(options):
 
@@ -23,7 +24,13 @@ def addOptions(options):
     options.weight = True
     options.final  = True
     options.allProcesses  = True
-    options.maxEntries = 10
+    #options.maxEntries = 10000
+
+    # signal scan
+    if options.signal:
+        options.var =  "mLSP:mGo"
+        options.bins = "30,0,1500,30,0,1500"
+        options.friendTrees = [("sf/t","FriendTrees_MC/evVarFriend_{cname}.root")]
 
 
 def analyzeReport(options):
@@ -112,7 +119,7 @@ if __name__ == "__main__":
     addMCAnalysisOptions(parser)
 
     # extra options for tty
-    parser.add_option("-m","--mca", dest="mcaFile",default="mca-Cards_test.txt",help="MCA sample list")
+    parser.add_option("-m","--mca", dest="mcaFile",default="mca-PAS.txt",help="MCA sample list")
     parser.add_option("-c","--cuts", dest="cutFile",default="trig_base.txt",help="Baseline cuts file")
     parser.add_option("--binname", dest="binname",default="test",help="Binname")
 
@@ -126,7 +133,6 @@ if __name__ == "__main__":
     ##parser.add_option("-o","--outdir", dest="outdir",default="test", help="out dir for cards")
 
     # running options
-    #parser.add_option("-v","--verbose", dest="verbose",default=False, action="store_true",help="verbose output")
     parser.add_option("-v","--verbose",  dest="verbose",  default=0,  type="int",    help="Verbosity level (0 = quiet, 1 = verbose, 2+ = more)")
     parser.add_option("--pretend", dest="pretend",default=False, action="store_true",help="pretend to do it")
 
@@ -137,6 +143,7 @@ if __name__ == "__main__":
 
     #makeShapeCards options
     parser.add_option("--asimov", dest="asimov", action="store_true", default=False, help="Make Asimov pseudo-data")
+    parser.add_option("--signal", dest="signal", action="store_true", default=False, help="Is signal scan")
     #parser.add_option("--dummy",  dest="dummyYieldsForZeroBkg", action="store_true", default=False, help="Set dummy yields such it corresponds to 0.01 for 4/fb");
     #parser.add_option("--ignoreEmptySignal",  dest="ignoreEmptySignal", action="store_true", default=False, help="Do not write out a datacard if the expected signal is less than 0.01");
 
@@ -146,46 +153,22 @@ if __name__ == "__main__":
     if options.verbose > 0:
         print 'Arguments', args
 
-    for lt_bin,lt_cut in binsLT.iteritems():
+    for bin in sorted(cutDict.keys()):
+        cuts = cutDict[bin]
+        options.bin = bin
+        options.cutsToAdd = cuts
 
-        htbins = []
+        if options.verbose > 0:
+            print 80*'#'
+            print 'Processing bin %s with cuts' %(bin),
+            for cut in cuts:
+                print cut[2],'+',
+            print
 
-        if lt_bin in ['LT1']:
-            htbins += ['HT0','HT1p2']
-        if lt_bin in ['LT1','LT2']:
-            htbins += ['HT0']
-        if lt_bin in ['LT2']:
-            htbins += ['HT1']
-        if lt_bin in ['LT2','LT3','LT4']:
-            htbins += ['HT2']
-        if lt_bin in ['LT3','LT4']:
-            htbins += ['HT0p1']
+        makeCards(options, args)
 
-        #for ht_bin,ht_cut in binsHT.iteritems():
-        for ht_bin in htbins:
-            ht_cut = binsHT[ht_bin]
+    if False:
+        options.bin = "signal"
+        makeCards(options, args)
 
-            nbbins = []
-
-            # Match NB bins
-            if lt_bin in ['LT1','LT2','LT3','LT4']:
-                nbbins += ['NB1'] # NB1 present in all LT bins
-            if lt_bin in ['LT1','LT2','LT3']:
-                nbbins += ['NB2','NB3'] # NB1 present in all LT bins
-            if lt_bin in ['LT4']:
-                nbbins += ['NB2p3'] # NB1 present in all LT bins
-
-            for nb_bin in nbbins:
-                nb_cut = binsNB[nb_bin]
-            #for nb_bin,nb_cut in binsNB.iteritems():
-
-                binname = "%s_%s_%s" %(lt_bin,ht_bin,nb_bin)
-                options.cutsToAdd = [("triggLT",lt_bin,lt_cut),("triggLT",ht_bin,ht_cut),("triggLT",nb_bin,nb_cut)]
-
-                if options.verbose > 0:
-                    print 80*'#'
-                    print 'Processing bin %s with cuts %s' %(binname,options.cutsToAdd)
-
-                options.bin = binname
-                #print options
-                makeCards(options, args)
+    print 'Finished'
--- CMGTools/TTHAnalysis/python/plotter/susy-1lep/RcsDevel/searchBins.py
+++ CMGTools/TTHAnalysis/python/plotter/susy-1lep/RcsDevel/searchBins.py
@@ -4,6 +4,7 @@ binsLT['LT1'] = '250 < LT && LT < 350'
 binsLT['LT2'] = '350 < LT && LT < 450'
 binsLT['LT3'] = '450 < LT && LT < 600'
 binsLT['LT4'] = '600 < LT'
+binsLT['LT3p4'] = '450 < LT'
 
 # HT bins
 binsHT = {}
@@ -13,16 +14,120 @@ binsHT['HT2'] = '1250 < HT'
 binsHT['HT1p2'] = '750 < HT'
 binsHT['HT0p1'] = '500 < HT && HT < 1250'
 
-#NB bins
+# NB bins
 binsNB = {}
-#binsNB['NB0'] = 'nBJet == 0'
+binsNB['NBi'] = 'nBJet >= 0'
+binsNB['NB0'] = 'nBJet == 0'
 binsNB['NB1'] = 'nBJet == 1'
 binsNB['NB2'] = 'nBJet == 2'
 binsNB['NB3'] = 'nBJet >= 3'
+binsNB['NB1i'] = 'nBJet >= 1'
 binsNB['NB2p3'] = 'nBJet >= 2'
 
+# NJ Bins
 
-# old
+################
+# MAKE CUT LISTS
+################
+binsNJ = {}
+#binsNJ['NJi'] = 'nJet >= 4'
+binsNJ['NJ45'] = '4 <= nJet && nJet <= 5'
+binsNJ['NJ68'] = '6 <= nJet && nJet <= 8'
+binsNJ['NJ9i'] = '9 <= nJet'
+
 cutDict = {}
-cutDict['HT1'] = ' -R "HT HT500 HT>500"'
-cutDict['HT2'] = ' -R "HT HT1000 HT>1000"'
+cutDictNJ45 = {}
+cutDictNJ68 = {}
+cutDictNJ9i = {}
+
+
+for nj_bin in ['NJ45','NJ68']:#binsNJ.iteritems():
+    nj_cut = binsNJ[nj_bin]
+
+    ltbins = ['LT1','LT2','LT3','LT4']
+
+    for lt_bin in ltbins:#binsLT.iteritems():
+        lt_cut = binsLT[lt_bin]
+
+        htbins = []
+
+        if lt_bin in ['LT1']:
+            htbins += ['HT0','HT1p2']
+        if lt_bin in ['LT1','LT2']:
+            htbins += ['HT0']
+        if lt_bin in ['LT2']:
+            htbins += ['HT1']
+        if lt_bin in ['LT2','LT3','LT4']:
+            htbins += ['HT2']
+        if lt_bin in ['LT3','LT4']:
+            htbins += ['HT0p1']
+
+        #for ht_bin,ht_cut in binsHT.iteritems():
+        for ht_bin in htbins:
+            ht_cut = binsHT[ht_bin]
+
+            nbbins = []
+
+            # Match NB bins
+            if lt_bin in ['LT1','LT2','LT3','LT4']:
+                nbbins += ['NB1'] # NB1 present in all NJ,LT bins
+            if lt_bin in ['LT4']:
+                nbbins += ['NB2p3'] # NB2p3 present in all NJ,LT bins
+
+            if lt_bin in ['LT1','LT2','LT3']:
+                # Signal region binning
+                if nj_bin in ['NJ68']:
+                    nbbins += ['NB2','NB3']
+                # Side band  binning
+                if nj_bin in ['NJ45']:
+                    nbbins += ['NB2p3']
+
+            for nb_bin in nbbins:
+                nb_cut = binsNB[nb_bin]
+
+                binname = "%s_%s_%s_%s" %(lt_bin,ht_bin,nb_bin,nj_bin)
+
+                cutDict[binname] = [("base",lt_bin,lt_cut),("base",ht_bin,ht_cut),("base",nb_bin,nb_cut),("base",nj_bin,nj_cut)]
+
+for nj_bin in ['NJ9i']:#binsNJ.iteritems():
+    nj_cut = binsNJ[nj_bin]
+
+    ltbins = ['LT1','LT2','LT3p4']
+
+    for lt_bin in ltbins:#binsLT.iteritems():
+        lt_cut = binsLT[lt_bin]
+
+        htbins = []
+
+
+        ### FIXME
+        if lt_bin in ['LT1']:
+            htbins += ['HT0p1','HT2']
+        if lt_bin in ['LT1','LT2']:
+            htbins += ['HT0']
+        if lt_bin in ['LT2']:
+            htbins += ['HT1']
+        if lt_bin in ['LT2','LT3','LT4']:
+            htbins += ['HT2']
+        if lt_bin in ['LT3','LT4']:
+            htbins += ['HT0p1']
+
+        #for ht_bin,ht_cut in binsHT.iteritems():
+        for ht_bin in htbins:
+            ht_cut = binsHT[ht_bin]
+
+            nbbins = []
+
+            # Match NB bins
+            if lt_bin in ['LT1','LT2']:
+                nbbins += ['NB1','NB2','NB3']
+
+            if lt_bin in ['LT3p4']:
+                nbbins += ['NB1i']
+
+            for nb_bin in nbbins:
+                nb_cut = binsNB[nb_bin]
+
+                binname = "%s_%s_%s_%s" %(lt_bin,ht_bin,nb_bin,nj_bin)
+
+                cutDictNJ9i[binname] = [("base",lt_bin,lt_cut),("base",ht_bin,ht_cut),("base",nb_bin,nb_cut),("base",nj_bin,nj_cut)]
