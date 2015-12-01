c0f719e1c8936443dd0e0af0efcdfa0cc7af8c85
Artur Lobanov <artur.lobanov@desy.de>
2015-12-01 14:30:37 +0100

    Add dbs command to sample macro

--- CMGTools/SUSYAnalysis/python/samples/makeSampleList.py
+++ CMGTools/SUSYAnalysis/python/samples/makeSampleList.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
+# command to make list
+# das_client --query="dataset=/SMS-T1tttt_mGluino-*TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15MiniAODv2-FastAsympt25ns_74X_mcRun2_asymptotic_v2-v1/MINIAODSIM" --key /afs/desy.de/user/l/lobanov/.globus/cms.proxy  --cert /afs/desy.de/user/l/lobanov/.globus/cms.proxy --limit 50
 
-listfile = "das_T1tttt_list.txt"
+listfile = "das_list_t1tttt.txt"
 
-oldPath = "_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15FSPremix-MCRUN2_74_V9-v1/MINIAODSIM"
-newPath = "_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15FSPremix-MCRUN2_74_V9-v1/MINIAODSIM"
+oldPath = "_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15MiniAODv2-FastAsympt25ns_74X_mcRun2_asymptotic_v2-v1/MINIAODSIM"
 
 compLine = "%s = kreator.makeMCComponent(\"%s\",\"%s\",\"CMS\",\".*root\")"
 
@@ -24,6 +25,6 @@ with open(listfile,"r") as flist:
 
         samps.append(samp)
         #print samp
-        #print compLine %(samp,samp,line)
+        print compLine %(samp,samp,line)
 
 print samps
