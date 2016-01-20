22bf4f61c4923cddfb4d253e3eef8c3290da287b
Artur Lobanov <artur.lobanov@desy.de>
2016-01-20 19:01:58 +0100

    Syst plot: make up and store dict in pickle

--- CMGTools/TTHAnalysis/python/plotter/susy-1lep/RcsDevel/plotSyst.py
+++ CMGTools/TTHAnalysis/python/plotter/susy-1lep/RcsDevel/plotSyst.py
@@ -4,7 +4,7 @@ import sys,os
 import makeYieldPlots as yp
 
 yp._batchMode = False
-yp._alpha = 0.75
+yp._alpha = 0.8
 
 if __name__ == "__main__":
 
@@ -25,31 +25,50 @@ if __name__ == "__main__":
         print "No pattern given!"
         exit(0)
     '''
-    pattern = "Syst"
 
-    #BinMask LTX_HTX_NBX_NJX for canvas names
-    basename = os.path.basename(pattern)
-    mask = basename.replace("*","X_")
+    ## Store dict in pickle file
+    storeDict = True
 
-    # Define storage
-    yds = yp.YieldStore("Sele")
-    paths = []
+    if storeDict == True and os.path.exists("allSysts.pck"):
 
-    # Add files
-    tptPath = "Yields/systs/topPt/MC/allSF_noPU/meth1A/merged/"; paths.append(tptPath)
-    puPath = "Yields/systs/PU/MC/allSF/meth1A/merged/"; paths.append(puPath)
-    wxsecPath = "Yields/systs/wXsec/MC/allSF_noPU/meth1A/merged/"; paths.append(wxsecPath)
-    dlConstPath = "Yields/systs/DLConst/merged"; paths.append(dlConstPath)
-    dlSlopePath = "Yields/systs/DLSlope/merged"; paths.append(dlSlopePath)
-    #jerPath = "Yields/systs/JER/merged"; paths.append(jerPath)
-    jerNoPath = "Yields/systs/JER_YesNo/merged"; paths.append(jerNoPath)
-    btagPath = "Yields/systs/btag/hadFlavour/fixXsec/allSF_noPU/meth1A/merged/"; paths.append(btagPath)
-    jecPath = "Yields/systs/JEC/MC/allSF_noPU/meth1A/merged/"; paths.append(jecPath)
+        print "#Loading saved yields from pickle!"
 
-    for path in paths:
-        yds.addFromFiles(path,("lep","sele"))
+        import cPickle as pickle
+        yds = pickle.load( open( "allSysts.pck", "rb" ) )
+        yds.showStats()
 
-    yds.showStats()
+    else:
+
+        print "#Reading yields from files!"
+
+        # Define storage
+        yds = yp.YieldStore("Sele")
+        paths = []
+
+        # Add files
+        tptPath = "Yields/systs/topPt/MC/allSF_noPU/meth1A/merged/"; paths.append(tptPath)
+        puPath = "Yields/systs/PU/MC/allSF/meth1A/merged/"; paths.append(puPath)
+        wxsecPath = "Yields/systs/wXsec/MC/allSF_noPU/meth1A/merged/"; paths.append(wxsecPath)
+        ttvxsecPath = "Yields/systs/TTVxsec/MC/allSF_noPU/meth1A/merged/"; paths.append(ttvxsecPath)
+        wpolPath = "Yields/systs/Wpol/MC/allSF_noPU/meth1A/merged/"; paths.append(wpolPath)
+        dlConstPath = "Yields/systs/DLConst/merged"; paths.append(dlConstPath)
+        dlSlopePath = "Yields/systs/DLSlope/merged"; paths.append(dlSlopePath)
+        jerPath = "Yields/systs/JER/merged"; paths.append(jerPath)
+        jerNoPath = "Yields/systs/JER_YesNo/merged"; paths.append(jerNoPath)
+        btagPath = "Yields/systs/btag/hadFlavour/fixXsec/allSF_noPU/meth1A/merged/"; paths.append(btagPath)
+        jecPath = "Yields/systs/JEC/MC/allSF_noPU/meth1A/merged/"; paths.append(jecPath)
+
+        for path in paths:
+            yds.addFromFiles(path,("lep","sele"))
+
+        yds.showStats()
+
+        print "#Saving yields to pickle"
+
+        # save to pickle
+        import cPickle as pickle
+        pickle.dump( yds, open( "allSysts.pck", "wb" ) )
+
     #print [name for name in yds.samples if ("syst" in name and "EWK" in name)]
     #exit(0)
 
@@ -60,7 +79,12 @@ if __name__ == "__main__":
 #    systs = ["JEC"]
 #    systs = ["btagHF","btagLF"]
 #    systs = ["btagLF","btagHF"]
-    systs = ["Wxsec","PU","JEC","btagHF","btagLF","topPt","DLConst","DLSlope","JER"]
+#    systs = ["DLConst","DLSlope"]
+
+#    systs = ["TTVxsec","Wxsec"]
+#    systs = ["Wpol","Wxsec"]
+#    systs = ["Wpol","Wxsec","PU","JEC","btagHF","btagLF","topPt","DLConst","DLSlope","JER","JERYesNo"]
+    systs = ["TTVxsec","Wpol","Wxsec","PU","JEC","btagHF","btagLF","topPt","DLConst","DLSlope"]
 
     systNames = {
         "btagLF" : "b-mistag (light)",
@@ -68,14 +92,24 @@ if __name__ == "__main__":
         "JEC" : "JEC",
         "topPt" : "Top p_{T}",
         "PU" : "PU",
-        "Wxsec" : "#sigma_{W}",
-        #"JER" : "JER",
-        "JER" : "JER Yes/No",
+        #"Wxsec" : "#sigma_{W}",
+        "Wxsec" : "W x-sec",
+        "TTVxsec" : "TTV x-sec",
+        "Wpol" : "W polar.",
+        "JER" : "JER",
+        "JERYesNo" : "JER Yes/No",
         "DLSlope" : "DL (Slope)",
         "DLConst" : "DL (Const)",
         }
 
-    sysCols = [2,4,7,8,3,9,6] + range(40,50)#[1,2,3] + range(4,10)
+    #sysCols = [2,4,7,8,3,9,6] + range(40,50)#[1,2,3] + range(4,10)
+    #sysCols = [50] + range(49,0,-2)#range(30,50,2)
+    #sysCols = range(40,100,1)#range(30,50,2)
+    #sysCols = range(35,100,3)
+    sysCols = range(28,100,2)
+    #sysCols = range(49,1,-2)
+    #sysCols = range(30,40,4) + range(40,100,3)
+    #sysCols = range(49,40,-2) + range(40,30,-3) + range(50,100,5)
 
     # Sample and variable
     samp = "EWK"
@@ -125,6 +159,7 @@ if __name__ == "__main__":
     canv = yp.plotHists(var+"_"+samp+"_Syst",[stack,sqHist],[hCentral,hCentralUncert],"TM", 1200, 600)
 #    canv = yp.plotHists(var+"_"+samp+"_Syst",[sqHist]+hists,[hCentral,hCentralUncert],"TM", 1200, 600)
 #    canv = yp.plotHists(var+"_"+samp+"_Stat",[stack,sqHist],hCentral,"TM", 1200, 600)
+
     canvs.append(canv)
     if not yp._batchMode: raw_input("Enter any key to exit")
 
@@ -132,9 +167,13 @@ if __name__ == "__main__":
     exts = [".pdf",".png",".root"]
     #exts = [".pdf"]
 
-    odir = "BinPlots/Syst/Combine/test/allSF_noPU_wJER_DL/Method1A/"
-    #odir = "BinPlots/Syst/btag/hadronFlavour/allSF_noPU/Method1B/"
+    #odir = "BinPlots/Syst/Combine/test/allSF_noPU_Wpol/Method1A/"
+    odir = "BinPlots/Syst/Combine/allSF_noPU_Wpol/Method1A/"
     if not os.path.isdir(odir): os.makedirs(odir)
+
+    ## Save hists
+    pattern = "Syst"
+    mask = pattern
 
     for canv in canvs:
         for ext in exts:
