497da2c8984cbd637274c99765a8a2787a0ab3e7
Artur Lobanov <artur.lobanov@desy.de>
2016-01-20 14:47:14 +0100

    Add Wpolar weights for W/TTJets into friends

--- CMGTools/SUSYAnalysis/python/tools/eventVars_1l_WeightsForSystematics.py
+++ CMGTools/SUSYAnalysis/python/tools/eventVars_1l_WeightsForSystematics.py
@@ -1,19 +1,183 @@
 from CMGTools.TTHAnalysis.treeReAnalyzer import *
 import ROOT
 
+ROOT.gROOT.ProcessLine(".L ../python/tools/WPolarizationVariation.C+")
+ROOT.gROOT.ProcessLine(".L ../python/tools/TTbarPolarization.C+")
+ROOT.gSystem.Load("libFWCoreFWLite.so")
+ROOT.AutoLibraryLoader.enable()
+
+def getLV(p4):
+    if p4 != None: return ROOT.LorentzVector(p4.Px(),p4.Py(),p4.Pz(),p4.E())
+    else: return p4
+
+def getGenWandLepton(event):
+
+    wP4 = None
+    lepP4 = None
+
+    genParts = [p for p in Collection(event,"GenPart","nGenPart")]
+    genLeps = filter(lambda l:abs(l.pdgId) in [11,13,15], genParts)
+
+    if len(genLeps) == 0:
+        print "no gen lepton found!"
+        return wP4, lepP4
+
+    lFromW = filter(lambda w:abs(w.motherId)==24, genLeps)
+
+    if len(lFromW) == 0:
+        print "no gen W found!", genLeps
+        return wP4, lepP4
+
+    elif len(lFromW)>1:
+        print 'this should not have happened'
+        return wP4, lepP4
+
+    elif len(lFromW) == 1:
+
+        genLep = lFromW[0]
+        genW = genParts[genLep.motherIndex]
+
+        wP4 = getLV(genW.p4())
+        lepP4 = getLV(genLep.p4())
+
+        #print genW.p4().M(),genLep.p4().M()
+        #print wP4.M(),lepP4.M()
+
+    return wP4, lepP4
+
+def getGenTopWLepton(event):
+
+    topP4 = None
+    wP4 = None
+    lepP4 = None
+
+    genParts = [p for p in Collection(event,"GenPart","nGenPart")]
+    genLeps = filter(lambda l:abs(l.pdgId) in [11,13,15], genParts)
+
+    if len(genLeps) == 0:
+        #print "no gen lepton found!" # happens in TTJets ;)
+        return topP4, wP4, lepP4
+
+    lFromW = filter(lambda w:abs(w.motherId)==24, genLeps)
+
+    if len(lFromW) == 0:
+        print "no gen W found!", genLeps
+        return topP4, wP4, lepP4
+
+    elif len(lFromW) > 2:
+        print "More than 2 W's found!"
+        return topP4, wP4, lepP4
+
+    elif len(lFromW) == 1:
+
+        genLep = lFromW[0]
+        genW = genParts[genLep.motherIndex]
+        genTop = genParts[genW.motherIndex]
+
+        topP4 = getLV(genTop.p4())
+        wP4 = getLV(genW.p4())
+        lepP4 = getLV(genLep.p4())
+
+        return topP4, wP4, lepP4
+
+    elif len(lFromW) == 2:
+        match = False
+
+        leadLep = Collection(event,"LepGood","nLepGood")[0]
+
+        for genLep in lFromW:
+            if leadLep.charge == genLep.charge:
+                match == True
+
+                genW = genParts[genLep.motherIndex]
+                genTop = genParts[genW.motherIndex]
+
+                topP4 = getLV(genTop.p4())
+                wP4 = getLV(genW.p4())
+                lepP4 = getLV(genLep.p4())
+
+                return topP4, wP4, lepP4
+
+        if not match:
+            print 'No match at all!'
+            return topP4, wP4, lepP4
+
+    return topP4, wP4, lepP4
+
+def getWPolWeights(event, sample):
+
+    wUp = 1
+    wDown = 1
+
+    if "TT" in sample: #W polarization in TTbar
+        topP4, wP4, lepP4 = getGenTopWLepton(event)
+
+        if topP4 != None:
+            #print topP4.M(), wP4.M(), lepP4.M()
+            cosTheta = ROOT.ttbarPolarizationAngle(topP4, wP4, lepP4)
+            #print cosTheta
+            wUp = (1. + 0.05*(1.-cosTheta)**2) * 1./(1.+0.05*2./3.) * (1./1.0323239521945559)
+            wDown = (1. - 0.05*(1.-cosTheta)**2) * 1./(1.-0.05*2./3.) * (1.034553190276963956)
+
+    elif "WJets" in sample: #W polarization in WJets
+        wP4, lepP4 = getGenWandLepton(event)
+
+        if wP4 != None:
+            #print wP4.M(), lepP4.M()
+            cosTheta = ROOT.WjetPolarizationAngle(wP4, lepP4)
+            #print cosTheta
+            wUp = (1. + 0.1*(1.-cosTheta)**2) * 1./(1.+0.1*2./3.) * (1./1.04923678332724659)
+            wDown = (1. - 0.1*(1.-cosTheta)**2) * 1./(1.-0.1*2./3.) * (1.05627060952003952)
+
+    #print wUp, wDown
+
+    return wUp, wDown
 
 class EventVars1LWeightsForSystematics:
     def __init__(self):
-        self.branches = ["GenTopPt", "GenAntiTopPt", "TopPtWeight", "GenTTBarPt", "GenTTBarWeight"
-                         ]
+        self.branches = [
+            # Top related
+            "GenTopPt", "GenAntiTopPt", "TopPtWeight", "GenTTBarPt", "GenTTBarWeight",
+            # W polarisation
+            "WpolWup","WpolWdown",
+            # PDF related
+            "pdfW","pdfW_Up","pdfW_Down"
+            ]
 
     def listBranches(self):
         return self.branches[:]
 
     def __call__(self,event,keyvals):
 
-        if event.isData: return {}
-        #
+        ret = {}
+
+        if event.isData: return ret
+
+        #### W polarisation
+        wPolWup, wPolWdown = getWPolWeights(event, self.sample)
+        ret['WpolWup'] = wPolWup
+        ret['WpolWdown'] = wPolWdown
+
+        ### PDF VARS
+        #"Pdfw","Pdfw_Up","Pdfw_Down"
+
+        pdfWup= 1
+        pdfWdown = 1
+        pdfWcentr = 1
+
+        '''
+        if hasattr(event,"LHEweight_wgt"):
+        pdfWmin = 99
+        pdfWmax = 0
+        #lheWgts = [w for w in Collection(event,"LHEweight_wgt","nLHEweight")]
+        '''
+
+        ret['pdfW'] = pdfWcentr
+        ret['pdfW_Up'] = pdfWup
+        ret['pdfW_Down'] = pdfWup
+
+
+        ### TOP RELATED VARS
         genParts = [l for l in Collection(event,"GenPart","nGenPart")]
 
         GenTopPt = -999
@@ -48,7 +212,7 @@ class EventVars1LWeightsForSystematics:
                 if GenTTBarPt>250: GenTTBarWeight= 0.80
                 if GenTTBarPt>400: GenTTBarWeight= 0.70
 
-        ret    =  { 'GenTopPt'   : GenTopPt } #initialize the dictionary with a first entry
+        ret['GenTopPt'] = GenTopPt  #initialize the dictionary with a first entry
         ret['GenAntiTopPt'] = GenAntiTopPt
         ret['TopPtWeight']  = TopPtWeight
         ret['GenTTBarPt']  = GenTTBarPt
